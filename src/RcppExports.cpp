352694f276d5f3642690a1db28f4401cb04c0f71
shounakch <shounak.chattopadhyay@gmail.com>
2025-11-19 23:14:24 +0100

    got rid of weird hello world code

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,49 +11,6 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _FABLE_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _FABLE_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _FABLE_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _FABLE_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // rGamma
 arma::vec rGamma(int n, double a, double b);
 RcppExport SEXP _FABLE_rGamma(SEXP nSEXP, SEXP aSEXP, SEXP bSEXP) {
@@ -253,10 +210,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_FABLE_rcpparma_hello_world", (DL_FUNC) &_FABLE_rcpparma_hello_world, 0},
-    {"_FABLE_rcpparma_outerproduct", (DL_FUNC) &_FABLE_rcpparma_outerproduct, 1},
-    {"_FABLE_rcpparma_innerproduct", (DL_FUNC) &_FABLE_rcpparma_innerproduct, 1},
-    {"_FABLE_rcpparma_bothproducts", (DL_FUNC) &_FABLE_rcpparma_bothproducts, 1},
     {"_FABLE_rGamma", (DL_FUNC) &_FABLE_rGamma, 3},
     {"_FABLE_SymmetrizeMatrix", (DL_FUNC) &_FABLE_SymmetrizeMatrix, 1},
     {"_FABLE_CPPsvd", (DL_FUNC) &_FABLE_CPPsvd, 2},
